7487831fa4adba1cf61831c9c3a8c8fd648322e8
Azrah Damon <qtilicousbbz@gmail.com>
2023-07-13 16:50:19 -0700

    Add script corrections to file 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	char message []= "Programming is like building a multilingual puzzle\n";
-	puts(message);
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return(0);
 }
