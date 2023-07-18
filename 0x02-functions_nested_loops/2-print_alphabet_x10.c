d7f65e80341556b030e43e41ef43177f61368fbe
Azrah Damon <qtilicousbbz@gmail.com>
2023-07-18 06:38:12 -0700

    Add function to file 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+void print_alphabet_x10(void)
+{
+    int i;
+    char letter;
+
+    for (i = 0; i < 10; i++)
+    {
+        letter = 'a';
+        while (letter <= 'z')
+        {
+            _putchar(letter);
+            letter++;
+        }
+        _putchar('\n');
+    }
+}
+
