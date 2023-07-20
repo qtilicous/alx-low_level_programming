e8c4e8d39535976b634106c8e10278cb7b15b9de
Azrah Damon <qtilicousbbz@gmail.com>
2023-07-20 11:22:07 -0700

    Add function to file 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints the numbers from 0 to 9 (except 2 and 4).
+ */
+
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		if (num != 2 && num != 4)
+		{
+			_putchar(num + '0'); /* Convert the number to its ASCII character */
+		}
+	}
+
+	_putchar('\n'); /* Print a new line after printing the numbers */
+}
+
