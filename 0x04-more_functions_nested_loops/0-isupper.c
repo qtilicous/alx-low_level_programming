f4089b5a7e982751145392d8e2c4bbf3066a1dec
Azrah Damon <qtilicousbbz@gmail.com>
2023-07-20 09:07:46 -0700

    Add function to file 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+/* File: 0-isupper.c */
+
+/**
+ * _isupper - Checks if a character is uppercase.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if c is uppercase, 0 otherwise.
+ */
+
+int _isupper(int c)
+{
+	/*
+	 * Check if the character is within the ASCII range of uppercase letters
+	 * ASCII range for uppercase letters: 'A' (65) to 'Z' (90)
+	 */
+	return ((c >= 'A' && c <= 'Z') ? 1 : 0);
+}
+
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory contains files with functions for the project 0x04-more_functions_nested_loops
