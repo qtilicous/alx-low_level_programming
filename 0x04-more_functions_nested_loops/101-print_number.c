00cbc672a95234d1078549c49423a77c6cf691b8
Azrah Damon <qtilicousbbz@gmail.com>
2023-07-20 14:45:12 -0700

    Add function for file 101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_number - Prints an integer.
+ * @n: The integer to be printed.
+ */
+
+void print_number(int n)
+{
+	int divisor = 1;
+	int temp;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	temp = n;
+	while (temp > 9)
+	{
+		divisor *= 10;
+		temp /= 10;
+	}
+
+	while (divisor >= 1)
+	{
+		_putchar((n / divisor) + '0');
+		n %= divisor;
+		divisor /= 10;
+	}
+}
+
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -22,4 +22,6 @@ void print_square(int size);
 
 void print_triangle(int size);
 
+void print_number(int n);
+
 #endif /* MAIN_H */
