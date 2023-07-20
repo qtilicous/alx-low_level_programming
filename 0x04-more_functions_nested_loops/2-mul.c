a728a0345f39f717138ea70c73c1935831188d48
Azrah Damon <qtilicousbbz@gmail.com>
2023-07-20 10:25:09 -0700

    Add function to file 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+/* File: mul.c */
+
+/**
+ * mul - Multiplies two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The result of the multiplication.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
