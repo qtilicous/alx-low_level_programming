2c09a23ac13ac6f56716a3266ac71b71c03a178f
Azrah Damon <qtilicousbbz@gmail.com>
2023-07-24 10:49:08 -0700

    Add function corrections to file reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
-/*
- * reset_to_98 - Takes a pointer to an int parameter
- * updates the value it points to 98
- * @n: pointer to an integer
+/**
+ * reset_to_98 - Takes a pointer to an int as parameter and updates the value
+ *               it points to to 98.
+ *
+ * @n: Pointer to an integer.
  */
 
 void reset_to_98(int *n)
