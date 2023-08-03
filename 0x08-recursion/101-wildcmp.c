ef1bc27ed02ffc951c768fb93bb37b391868e657
Azrah Damon <qtilicousbbz@gmail.com>
2023-08-02 23:10:34 -0700

    Add function correction to file 101-wildcmo.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,6 +1,46 @@
 #include "main.h"
 
 /**
+ * wildcmp_helper - Helper function to compare two strings recursively.
+ *
+ * @s1: Pointer to the first input string.
+ * @s2: Pointer to the second input string.
+ *
+ * Return: 1 if the strings can be considered identical, otherwise 0.
+ */
+int wildcmp_helper(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+		if (*s2 == '\0')
+			return (1);
+		if (*s2 == '*')
+			return (wildcmp_helper(s1, s2 + 1));
+		return (0);
+	}
+
+	if (*s2 == '\0')
+		return (0);
+
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+			return (wildcmp_helper(s1, s2 + 1));
+		else if (*(s2 + 1) == '\0')
+			return (1);
+
+		if (wildcmp_helper(s1, s2 + 1))
+			return (1);
+		return (wildcmp_helper(s1 + 1, s2));
+	}
+
+	if (*s1 == *s2)
+		return (wildcmp_helper(s1 + 1, s2 + 1));
+
+	return (0);
+}
+
+/**
  * wildcmp - Compares two strings and checks if they can be identical.
  *
  * @s1: Pointer to the first input string.
@@ -10,25 +50,6 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (*s2 == '*')
-	{
-		if (*(s2 + 1) == '*')
-			return (wildcmp(s1, s2 + 1));
-		else if (*(s2 + 1) == '\0')
-			return (1);
-
-		while (*s1 != '\0')
-		{
-			if (wildcmp(s1, s2 + 1))
-				return (1);
-			s1++;
-		}
-		return (0);
-	}
-
-	if (*s1 == *s2)
-		return (wildcmp(s1 + 1, s2 + 1));
-
-	return (0);
+	return (wildcmp_helper(s1, s2));
 }
 
