4f4b864e65ca7589a7fc1658b3f1a3104492435d
Azrah Damon <qtilicousbbz@gmail.com>
2023-08-15 13:16:55 -0700

    Add function correction to file 4-new_dog.c

--- 0x0E-structures_typedef/4-main.c
+++ 0x0E-structures_typedef/4-main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "dog.h"
 
 /**
@@ -12,17 +11,7 @@ int main(void)
 	dog_t *my_dog;
 
 	my_dog = new_dog("Poppy", 3.5, "Bob");
-	if (my_dog != NULL)
-	{
-		printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
-		free(my_dog->name);
-		free(my_dog->owner);
-		free(my_dog);
-	}
-	else
-	{
-		printf("Failed to create a new dog.\n");
-	}
+	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
 	return (0);
 }
 
