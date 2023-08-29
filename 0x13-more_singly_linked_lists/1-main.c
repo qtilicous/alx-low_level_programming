c38af1f653b7d2c85da54d6f30caa3beafe4b0b3
qtilicous <qtilicousbbz@gmail.com>
2023-08-29 12:23:52 -0700

    Add function to file 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-main.c
+++ 0x13-more_singly_linked_lists/1-main.c
@@ -17,15 +17,15 @@ int main(void)
 
 	head = &hello;
 	new = malloc(sizeof(listint_t));
-		if (new == NULL)
-		{
-			printf("Error\n");
-			return (1);
-		}
+	if (new == NULL)
+	{
+		printf("Error\n");
+		return (1);
+	}
 	new->n = 9;
 	new->next = head;
 	head = new;
-	n = print_listint(head);
+	n = listint_len(head);
 	printf("-> %lu elements\n", n);
 	free(new);
 	return (0);
