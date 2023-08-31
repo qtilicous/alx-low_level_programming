87ecffc8c750ca4d53ee09bdfd51390fbbe5f026
qtilicous <qtilicousbbz@gmail.com>
2023-08-31 15:20:24 -0700

    Add function to file 5-flip_bits.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * flip_bits - Returns the number of bits needed to flip to get from n to m.
+ * @n: The source number.
+ * @m: The target number.
+ *
+ * Return: The number of bits needed to flip.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int diff = n ^ m;
+	unsigned int count = 0;
+
+	while (diff > 0)
+	{
+		count += diff & 1;
+		diff >>= 1;
+	}
+
+	return (count);
+}
+
