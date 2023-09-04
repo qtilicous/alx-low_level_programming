3dc8a1b075cf0ef91dfee8b4c83e934cfcd290d6
qtilicous <qtilicousbbz@gmail.com>
2023-09-04 10:33:32 -0700

    Add function to file 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,7 +2,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-
+/**
+ * read_textfile - Read and print a text file to the standard output.
+ * @filename: The name of the file to read.
+ * @letters: The number of letters to read and print.
+ * Return: The actual number of letters read and printed, or 0 on error.
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
@@ -12,12 +17,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
-
 	fd = open(filename, O_RDONLY);
 
 	if (fd == -1)
 		return (0);
-
 	buffer = malloc(letters);
 
 	if (buffer == NULL)
@@ -25,7 +28,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(fd);
 		return (0);
 	}
-
 	bytes_read = read(fd, buffer, letters);
 
 	if (bytes_read == -1)
@@ -34,7 +36,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(fd);
 		return (0);
 	}
-
 	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 
 	if (bytes_written == -1 || bytes_written != bytes_read)
@@ -43,7 +44,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(fd);
 		return (0);
 	}
-
 	free(buffer);
 	close(fd);
 	return (bytes_written);
