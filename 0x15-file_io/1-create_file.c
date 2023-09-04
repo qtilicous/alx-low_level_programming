d47db156e39c1bc143f0a69046e8b7b15f0004ed
qtilicous <qtilicousbbz@gmail.com>
2023-09-04 11:01:11 -0700

    Add function to file 1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include "main.h"
+/**
+ * create_file - Create a file with specified permissions and write content.
+ * @filename: The name of the file to create.
+ * @text_content: The NULL-terminated string to write to the file.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, bytes_written;
+	mode_t mode = S_IRUSR | S_IWUSR;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, mode);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		bytes_written = write(fd, text_content, strlen(text_content));
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
+
