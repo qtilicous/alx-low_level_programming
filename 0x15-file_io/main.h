73211fb72aa2166c882bfa378502617606b44558
qtilicous <qtilicousbbz@gmail.com>
2023-09-22 11:02:15 -0700

    still trying to fix 3-cp.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stddef.h>
 #include <sys/types.h>
 #include "main.h"
 #include <stdio.h>
@@ -8,7 +9,6 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/stat.h>
-#include <stddef.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
